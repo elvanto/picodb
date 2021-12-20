fa16a1c5fc81f6df59d81f625a772087234efb9d
StewPoll <administrator@tetraetc.com>
2021-12-20 13:51:12 +1000

    Fix sqlite unit tests for not like

--- tests/SqliteTableTest.php
+++ tests/SqliteTableTest.php
@@ -65,21 +65,21 @@ class SqliteTableTest extends \PHPUnit\Framework\TestCase
     public function testLike()
     {
         $query = $this->db->table('test')->like('a', 'test');
-        $this->assertEquals('SELECT * FROM `test` WHERE `a` LIKE ?', $query->buildSelectQuery());
+        $this->assertEquals('SELECT * FROM "test" WHERE "a" LIKE ?', $query->buildSelectQuery());
         $this->assertEquals('a', $query->getConditionBuilder()->getValues()[0]);
     }
 
     public function testIlike()
     {
         $query = $this->db->table('test')->like('a', 'test');
-        $this->assertEquals('SELECT * FROM `test` WHERE `a` LIKE ?', $query->buildSelectQuery());
+        $this->assertEquals('SELECT * FROM "test" WHERE "a" LIKE ?', $query->buildSelectQuery());
         $this->assertEquals('a', $query->getConditionBuilder()->getValues()[0]);
     }
 
     public function testNotLike()
     {
         $query = $this->db->table('test')->like('a', 'test');
-        $this->assertEquals('SELECT * FROM `test` WHERE `a` NOT LIKE ?', $query->buildSelectQuery());
+        $this->assertEquals('SELECT * FROM "test" WHERE "a" NOT LIKE ?', $query->buildSelectQuery());
         $this->assertEquals('a', $query->getConditionBuilder()->getValues()[0]);
     }
 
