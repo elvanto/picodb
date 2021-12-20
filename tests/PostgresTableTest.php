a1366c72d98c1fdb52fa6af50d0e9d581e231822
StewPoll <administrator@tetraetc.com>
2021-12-20 13:55:36 +1000

    Fix spacing

--- tests/PostgresTableTest.php
+++ tests/PostgresTableTest.php
@@ -70,21 +70,21 @@ class PostgresTableTest extends \PHPUnit\Framework\TestCase
     public function testLike()
     {
         $query = $this->db->table('test')->like('a', 'test');
-        $this->assertEquals('SELECT * FROM `test` WHERE `a` LIKE ?', $query->buildSelectQuery());
+        $this->assertEquals('SELECT * FROM "test"   WHERE "a" LIKE ?', $query->buildSelectQuery());
         $this->assertEquals('a', $query->getConditionBuilder()->getValues()[0]);
     }
 
     public function testIlike()
     {
         $query = $this->db->table('test')->like('a', 'test');
-        $this->assertEquals('SELECT * FROM `test` WHERE `a` ILIKE ?', $query->buildSelectQuery());
+        $this->assertEquals('SELECT * FROM "test"   WHERE "a" ILIKE ?', $query->buildSelectQuery());
         $this->assertEquals('a', $query->getConditionBuilder()->getValues()[0]);
     }
 
     public function testNotLike()
     {
         $query = $this->db->table('test')->like('a', 'test');
-        $this->assertEquals('SELECT * FROM `test` WHERE `a` NOT LIKE ?', $query->buildSelectQuery());
+        $this->assertEquals('SELECT * FROM "test"   WHERE "a" NOT LIKE ?', $query->buildSelectQuery());
         $this->assertEquals('a', $query->getConditionBuilder()->getValues()[0]);
     }
 
