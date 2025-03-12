6eb705bcc0c013ea896cb46e643eadc4328c3fbd
b-hayes <bradley.hayes@yourgiving.co>
2025-03-12 20:07:31 +1100

    keep mysql container for the next run

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -8,6 +8,7 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <php_version> <mysql_version>"
     echo "Example: $0 7.4 5.7"
     echo "Example: $0 8.1 8.0"
+    echo "Note that MySql container will remain running to make conceive runs faster."
     exit 1
 fi
 
@@ -21,20 +22,31 @@ CURRENT_FOLDER=$(basename "$PWD")
 PHP_CONTAINER_NAME="${CURRENT_FOLDER}-php-${PHP_VERSION}"
 MYSQL_CONTAINER_NAME="${CURRENT_FOLDER}-mysql-${MYSQL_VERSION}"
 
-# Pulling images.                   # Fallback to amd64 emulation when no image exists for apple silicone.
-docker pull php:$PHP_VERSION-cli || docker pull --platform linux/amd64 php:$PHP_VERSION-cli
-docker pull mysql:$MYSQL_VERSION || docker pull --platform linux/amd64 mysql:$MYSQL_VERSION
+# Check if the PHP image exists, if not pull it
+if ! docker image inspect php:$PHP_VERSION-cli > /dev/null 2>&1; then
+    docker pull php:$PHP_VERSION-cli || docker pull --platform linux/amd64 php:$PHP_VERSION-cli
+fi
 
-# Run MySQL container
-docker run --name $MYSQL_CONTAINER_NAME -d \
-  -e MYSQL_ROOT_PASSWORD=rootpassword \
-  -e MYSQL_DATABASE=picodb \
-  -p 3306:3306 \
-  mysql:$MYSQL_VERSION
+# Check if the MySQL image exists, if not pull it
+if ! docker image inspect mysql:$MYSQL_VERSION > /dev/null 2>&1; then
+    docker pull mysql:$MYSQL_VERSION || docker pull --platform linux/amd64 mysql:$MYSQL_VERSION
+fi
 
-# Wait for MySQL to start
-echo "Waiting for MySQL to start..."
-sleep 20
+# Check if MySQL container is already running
+if [ -z "$(docker ps -a -q -f name=$MYSQL_CONTAINER_NAME)" ]; then
+    # Run MySQL container
+    docker run --name $MYSQL_CONTAINER_NAME -d \
+      -e MYSQL_ROOT_PASSWORD=rootpassword \
+      -e MYSQL_DATABASE=picodb \
+      -p 3306:3306 \
+      mysql:$MYSQL_VERSION
+
+    # Wait for MySQL to start only if it was just created
+    echo "Waiting for MySQL to start..."
+    sleep 20
+else
+    echo "MySQL container $MYSQL_CONTAINER_NAME already exists."
+fi
 
 # Disable stop on error from here.
 set +e
@@ -48,10 +60,6 @@ docker run --name $PHP_CONTAINER_NAME --rm \
 
 EitCode=$?
 
-# Clean up
-docker stop $MYSQL_CONTAINER_NAME
-docker rm $MYSQL_CONTAINER_NAME
-# PHP container should automatically stop and remove itself.
 
 if [ $EitCode -ne 0 ]; then
     echo "❌ Tests failed for PHP $PHP_VERSION and MySQL $MYSQL_VERSION"
